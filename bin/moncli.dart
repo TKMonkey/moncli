f0aac24fc25e12b15bacd390e29962da620171e5
jamescardona11 <jjcardonao@gmail.com>
2021-04-10 16:01:09 -0500

    Init project

---
+++ bin/moncli.dart
@@ -0,0 +1,5 @@
+import 'package:moncli/moncli.dart' as moncli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${moncli.calculate()}!');
+}
---
+++ test/moncli_test.dart
@@ -0,0 +1,8 @@
+import 'package:moncli/moncli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
