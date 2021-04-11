a126e6f551e258233ad9f27c51976b88453f5d90
jamescardona11 <jjcardonao@gmail.com>
2021-04-10 21:02:19 -0500

    Default information for the cli cnsole

--- bin/moncli.dart
+++ bin/moncli.dart
@@ -1,4 +1,5 @@
 import 'package:moncli/moncli.dart' as moncli;
+import 'package:args/args.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${moncli.calculate()}!');
--- test/moncli_test.dart
+++ test/moncli_test.dart
@@ -1,8 +1,8 @@
 import 'package:moncli/moncli.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
