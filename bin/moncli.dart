7ca41c2870776e24a7bae5cb6df1f3359cb24eb7
jamescardona11 <jjcardonao@gmail.com>
2021-04-10 21:51:52 -0500

    Add basic configuration workflow for moncli

--- bin/moncli.dart
+++ bin/moncli.dart
@@ -1,6 +1,18 @@
-import 'package:moncli/moncli.dart' as moncli;
 import 'package:args/args.dart';
+import 'utils/utils.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${moncli.calculate()}!');
+  final moncli = configureCommand(arguments);
+  final runner = moncli.runner;
+
+  var hasCommand = runner.commands.keys.any((x) => arguments.contains(x));
+
+  if (hasCommand) {
+    executeCommand(moncli, arguments);
+  } else {
+    var parser = ArgParser();
+    parser = runner.argParser;
+    var results = parser.parse(arguments);
+    executeOptions(results, arguments, moncli);
+  }
 }
