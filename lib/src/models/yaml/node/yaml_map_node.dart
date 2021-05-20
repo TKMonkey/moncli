1e461aeeb2986ddc0d69656f30ec3b376be3d37b
Juan E Quintero R <juan@57blocks.com>
2021-05-20 11:29:29 -0500

    create YamlMapNode

---
+++ lib/src/models/yaml/node/yaml_map_node.dart
@@ -0,0 +1,63 @@
+import 'package:moncli/src/models/node/i_map_node.dart';
+import 'package:moncli/src/models/node/i_node.dart';
+import 'package:moncli/src/models/node/node_validator.dart';
+
+class YamlMapNode implements IMapNode {
+  static const sEmpty = YamlMapNode(<String, INode>{});
+
+  final Map<String, INode> _value;
+
+  const YamlMapNode(this._value);
+
+  @override
+  IMapNode get empty => sEmpty;
+
+  @override
+  Map<String, INode> get value => Map.unmodifiable(_value);
+
+  @override
+  String toSerializedString(int currentIndentation, bool isTopLevel) {
+    var firstFont = false;
+
+    final StringBuffer ss = StringBuffer();
+    int newIndentation = currentIndentation;
+    if (!isTopLevel) {
+      ss.writeln();
+      newIndentation += 2;
+    }
+
+    _value.forEach(
+      (k, v) {
+        if (k == 'fonts' && v is List && !firstFont) {
+          ss.writeln();
+          firstFont = true;
+        }
+        _writeIndent(newIndentation, ss);
+        ss
+          ..write(k)
+          ..write(': ')
+          ..write(
+            v.toSerializedString(
+              newIndentation,
+              false,
+            ),
+          );
+      },
+    );
+
+    return ss.toString();
+  }
+
+  @override
+  String toString() {
+    return 'YamlMapNode{_value: $_value}';
+  }
+
+  @override
+  NodeValidator validate(NodeValidator elementValidator) {
+    return elementValidator;
+  }
+
+  void _writeIndent(int indentation, StringSink ss) =>
+      ss.write(' ' * indentation);
+}
