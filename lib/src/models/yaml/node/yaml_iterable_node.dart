216b045f7bbc98c376a521660d1d50440534604a
Juan E Quintero R <juan@57blocks.com>
2021-05-20 11:29:24 -0500

    create YamlIterableNode

---
+++ lib/src/models/yaml/node/yaml_iterable_node.dart
@@ -0,0 +1,61 @@
+import 'package:moncli/src/models/node/i_iterable_node.dart';
+import 'package:moncli/src/models/node/i_node.dart';
+import 'package:moncli/src/models/node/node_validator.dart';
+import 'package:moncli/src/models/yaml/node/yaml_node_factory.dart';
+
+class YamlIterableNode<T> implements IIterableNode<T> {
+  static YamlIterableNode<T> sEmpty<T>() =>
+      YamlIterableNode<T>(Iterable<INode<T>>.empty());
+
+  final Iterable<INode<T>> _value;
+
+  const YamlIterableNode(this._value);
+
+  @override
+  IIterableNode<R> castTo<R>() {
+    return YamlNodeFactory.sInstance
+        .createIterableNode<R>(_value.map((item) => item.value as R));
+  }
+
+  @override
+  IIterableNode<T> empty() => sEmpty();
+
+  @override
+  Iterable<INode<T>> get value => List.unmodifiable(_value);
+
+  @override
+  String toSerializedString(int currentIndentation, bool isTopLevel) {
+    final StringBuffer ss = StringBuffer();
+    int newIndentation = currentIndentation;
+    if (!isTopLevel) {
+      ss.writeln();
+      newIndentation += 2;
+    }
+    for (final node in _value) {
+      _writeIndent(newIndentation, ss);
+      ss
+        ..write('- ')
+        ..write(
+          node.toSerializedString(
+            newIndentation,
+            false,
+          ),
+        );
+    }
+
+    return ss.toString();
+  }
+
+  @override
+  String toString() {
+    return 'YamlIterableNode{_value: $_value}';
+  }
+
+  @override
+  NodeValidator validate(NodeValidator elementValidator) {
+    return elementValidator;
+  }
+
+  void _writeIndent(int indentation, StringSink ss) =>
+      ss.write(' ' * indentation);
+}
