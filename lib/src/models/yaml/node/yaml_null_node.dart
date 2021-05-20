6dcddf54984dcb7b0a5142dabca9e347b75688a0
Juan E Quintero R <juan@57blocks.com>
2021-05-20 11:29:49 -0500

    create YamlNullNode

---
+++ lib/src/models/yaml/node/yaml_null_node.dart
@@ -0,0 +1,23 @@
+import 'package:moncli/src/models/node/i_null_node.dart';
+import 'package:moncli/src/models/node/node_validator.dart';
+
+class YamlNullNode implements INullNode {
+  @override
+  void get value {}
+
+  @override
+  String toSerializedString(int currentIndentation, bool isTopLevel) {
+    return '';
+  }
+
+  @override
+  String toString() {
+    return 'YamlNullNode{}';
+  }
+
+  @override
+  NodeValidator validate(NodeValidator elementValidator) => elementValidator
+    ..reason = elementValidator.isRequired
+        ? '${elementValidator.key} is required'
+        : "";
+}
