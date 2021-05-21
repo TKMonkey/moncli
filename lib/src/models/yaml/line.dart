4a7b0f75ad15ac96da76692912f4f301c3a6a7ab
Juan E Quintero R <juan@57blocks.com>
2021-05-21 07:26:28 -0500

    refactor: move YamlLine to file writing into YamlLine

--- lib/src/models/yaml/line.dart
+++ lib/src/models/yaml/line.dart
@@ -34,12 +34,19 @@ abstract class YamlLine {
     }
   }
 
+  void writeIntoSink(StringSink sink, Yaml yaml);
+
   @override
   String toString() => line;
 }
 
 class EmptyLine extends YamlLine {
   const EmptyLine() : super('');
+
+  @override
+  void writeIntoSink(StringSink sink, Yaml yaml) {
+    sink.writeln(line);
+  }
 }
 
 class KeyLine extends YamlLine {
@@ -64,12 +71,42 @@ class KeyLine extends YamlLine {
   String _getValue(List<String> split) => split.length > 1 ? split[1] : '';
 
   String _getOther(List<String> split) => split.length > 2 ? split[2] : '';
+
+  @override
+  void writeIntoSink(StringSink sink, Yaml yaml) {
+    final innerSink = StringBuffer();
+    _writeYamlString(
+        YamlNodeFactory.sInstance.createMapNode({key: yaml[key]}), innerSink);
+
+    /// moves text to be inline with [hyphen '-']
+    sink.write(innerSink.toString().replaceAll(RegExp(r'-\s\n\s*'), '- '));
+  }
+
+  /// Serializes [node] into a String and writes it to the [sink].
+  void _writeYamlString(INode node, StringSink sink) {
+    _writeYamlType(node, 0, sink, true);
+  }
+
+  void _writeYamlType(INode node, int indent, StringSink ss, bool isTopLevel) {
+    final yamlString = node.toSerializedString(indent, isTopLevel);
+    ss.write(yamlString);
+  }
 }
 
 class CommentLine extends YamlLine {
   const CommentLine(String line) : super(line);
+
+  @override
+  void writeIntoSink(StringSink sink, Yaml yaml) {
+    sink.writeln(line);
+  }
 }
 
 class SubLine extends YamlLine {
   const SubLine(String line) : super(line);
+
+  @override
+  void writeIntoSink(StringSink sink, Yaml yaml) {
+    sink.writeln(line);
+  }
 }
--- lib/src/models/yaml/mixin_yaml_printer.dart
+++ lib/src/models/yaml/mixin_yaml_printer.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:moncli/src/base/constants.dart';
-import 'package:moncli/src/models/node/i_node.dart';
 import 'package:moncli/src/models/yaml/line.dart';
-import 'package:moncli/src/models/yaml/node/yaml_node_factory.dart';
 import 'package:moncli/src/models/yaml/yaml.dart';
 
 mixin YamlPrinterMixin {
@@ -16,29 +14,9 @@ mixin YamlPrinterMixin {
   ) {
     final sb = StringBuffer();
 
-    for (final node in listLines) {
-      if (node is KeyLine) {
-        final sbk = StringBuffer();
-        writeYamlString(
-            YamlNodeFactory.sInstance.createMapNode({node.key: yaml[node.key]}),
-            sbk);
-
-        /// moves text to be inline with [hyphen '-']
-        sb.write(sbk.toString().replaceAll(RegExp(r'-\s\n\s*'), '- '));
-      } else {
-        sb.writeln(node.line);
-      }
+    for (final line in listLines) {
+      line.writeIntoSink(sb, yaml);
     }
     File(outputPubPath).writeAsStringSync(sb.toString());
-  }
-
-  /// Serializes [node] into a String and writes it to the [sink].
-  void writeYamlString(INode node, StringSink sink) {
-    _writeYamlType(node, 0, sink, true);
-  }
-
-  void _writeYamlType(INode node, int indent, StringSink ss, bool isTopLevel) {
-    final yamlString = node.toSerializedString(indent, isTopLevel);
-    ss.write(yamlString);
   }
 }
