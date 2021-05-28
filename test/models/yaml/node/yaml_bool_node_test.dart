a4c6d66ac12ad0121577ab6bc2f3c54a77b87108
Juan E Quintero R <juan@57blocks.com>
2021-05-28 08:06:09 -0500

    test: add test to YamlBoolNode

---
+++ test/models/yaml/node/yaml_bool_node_test.dart
@@ -0,0 +1,81 @@
+import 'package:moncli/src/models/yaml/node/yaml_bool_node.dart';
+import 'package:test/test.dart';
+
+class Input {
+  final bool value;
+  final int currentIndentation;
+  final bool topLevelValue;
+
+  Input(
+      {required this.value,
+      required this.currentIndentation,
+      required this.topLevelValue});
+}
+
+void main() {
+  group("YamlBoolNode", () {
+    group("Constructor", () {
+      test("true param, value must be true", () {
+        // Arrange
+        final yamlBoolNode = YamlBoolNode(true);
+        // Act
+        final value = yamlBoolNode.value;
+
+        // Assert
+        expect(value, isTrue);
+      });
+
+      test("false param, value must be false", () {
+        // Arrange
+        final yamlBoolNode = YamlBoolNode(false);
+        // Act
+        final value = yamlBoolNode.value;
+
+        // Assert
+        expect(value, isFalse);
+      });
+    });
+
+    group("toSerializedString", () {
+      final inputAndExpected = {
+        Input(value: true, currentIndentation: 0, topLevelValue: true):
+            "true\n",
+        Input(value: true, currentIndentation: 0, topLevelValue: false):
+            "true\n",
+        Input(value: true, currentIndentation: 10, topLevelValue: true):
+            "true\n",
+        Input(value: true, currentIndentation: 10, topLevelValue: false):
+            "true\n",
+        Input(value: false, currentIndentation: 0, topLevelValue: true):
+            "false\n",
+        Input(value: false, currentIndentation: 0, topLevelValue: false):
+            "false\n",
+        Input(value: false, currentIndentation: 10, topLevelValue: true):
+            "false\n",
+        Input(value: false, currentIndentation: 10, topLevelValue: false):
+            "false\n",
+      };
+
+      // ignore: cascade_invocations
+      inputAndExpected.forEach((key, value) {
+        final nodeValue = key.value;
+        final currentIndentation = key.currentIndentation;
+        final topLevelValue = key.topLevelValue;
+        final expected = value;
+
+        test(
+            "$value value, $currentIndentation currentIndentation and $topLevelValue topLevelValue, must return $expected",
+            () {
+          // Arrange
+          final yamlBoolNode = YamlBoolNode(nodeValue);
+          // Act
+          final serializedString = yamlBoolNode.toSerializedString(
+              currentIndentation, topLevelValue);
+
+          // Assert
+          expect(serializedString, expected);
+        });
+      });
+    });
+  });
+}
