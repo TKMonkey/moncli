2cbb8ef788e31cb75028a99ff6032000e1d89059
Juan E Quintero R <juan@57blocks.com>
2021-06-03 07:40:34 -0500

    test: use const constructor, add validate and value tests

--- lib/src/models/yaml/node/yaml_null_node.dart
+++ lib/src/models/yaml/node/yaml_null_node.dart
@@ -2,17 +2,14 @@ import 'package:moncli/src/models/node/i_null_node.dart';
 import 'package:moncli/src/models/node/node_validator.dart';
 
 class YamlNullNode implements INullNode {
+  const YamlNullNode();
+
   @override
   void get value {}
 
   @override
   String toSerializedString(int currentIndentation, bool isTopLevel) {
     return '\n';
-  }
-
-  @override
-  String toString() {
-    return 'YamlNullNode{}';
   }
 
   @override
--- test/models/yaml/node/yaml_null_node_test.dart
+++ test/models/yaml/node/yaml_null_node_test.dart
@@ -1,3 +1,4 @@
+import 'package:moncli/src/models/node/node_validator.dart';
 import 'package:moncli/src/models/yaml/node/yaml_null_node.dart';
 import 'package:test/test.dart';
 
@@ -8,8 +9,59 @@ class Input {
   Input({required this.currentIndentation, required this.topLevelValue});
 }
 
+class MockNodeValidator extends NodeValidator {
+  dynamic? receivedValue = 1;
+
+  MockNodeValidator(
+      {required String key, Iterable<String> validValues = const []})
+      : super(key: key, validValues: validValues);
+
+  @override
+  void validateValue(value) {
+    receivedValue = value;
+    super.validateValue(value);
+  }
+}
+
 void main() {
   group("YamlNullNode", () {
+    test("value should be void", () {
+      // Arrange
+      const yamlNullNode = YamlNullNode();
+
+      // Act
+      // ignore: cascade_invocations
+      yamlNullNode.value;
+    });
+
+    group("validate", () {
+      test("should call node validator validateValue with null value", () {
+        // Arrange
+        final nodeValidator = MockNodeValidator(key: "aKey");
+        const yamlNullNode = YamlNullNode();
+
+        // Act
+        // ignore: cascade_invocations
+        yamlNullNode.validate(nodeValidator);
+
+        // Assert
+        expect(nodeValidator.receivedValue, isNull);
+      });
+
+      test("validation not passed, should return invalid node validator", () {
+        // Arrange
+        final nodeValidator = MockNodeValidator(key: "aKey");
+        const yamlNullNode = YamlNullNode();
+
+        // Act
+        // ignore: cascade_invocations
+        final newNodeValidator = yamlNullNode.validate(nodeValidator);
+
+        // Assert
+        expect(newNodeValidator.isValid, isFalse);
+      });
+    });
+
     group("toSerializedString", () {
       final inputAndExpected = {
         Input(currentIndentation: 0, topLevelValue: true): "\n",
@@ -28,7 +80,7 @@ void main() {
             "$currentIndentation currentIndentation and $topLevelValue topLevelValue, must return $expected",
             () {
           // Arrange
-          final yamlNullNode = YamlNullNode();
+          const yamlNullNode = YamlNullNode();
 
           // Act
           final serializedString = yamlNullNode.toSerializedString(
