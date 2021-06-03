8ad4db6f04140ec8e6827b7e6af4e1c459296c7b
Juan E Quintero R <juan@57blocks.com>
2021-06-03 07:22:36 -0500

    test: use const

--- test/models/yaml/node/yaml_double_node_test.dart
+++ test/models/yaml/node/yaml_double_node_test.dart
@@ -31,7 +31,7 @@ void main() {
   group("Constructor", () {
     test("positive param, value must be the received param", () {
       // Arrange
-      final yamlDoubleNode = YamlDoubleNode(10.5);
+      const yamlDoubleNode = YamlDoubleNode(10.5);
       // Act
       final value = yamlDoubleNode.value;
       // Assert
@@ -40,7 +40,7 @@ void main() {
 
     test("negative param, value must be the received param", () {
       // Arrange
-      final yamlDoubleNode = YamlDoubleNode(-10.5);
+      const yamlDoubleNode = YamlDoubleNode(-10.5);
       // Act
       final value = yamlDoubleNode.value;
       // Assert
@@ -49,7 +49,7 @@ void main() {
 
     test("zero param, value must be the received param", () {
       // Arrange
-      final yamlDoubleNode = YamlDoubleNode(0.5);
+      const yamlDoubleNode = YamlDoubleNode(0.5);
       // Act
       final value = yamlDoubleNode.value;
       // Assert
