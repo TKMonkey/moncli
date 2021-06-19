907e104e28a23696195d678d6baea2872d425c56
Juan E Quintero R <juan@57blocks.com>
2021-06-19 08:27:33 -0500

    feat: create ICreateTemplatesDirectory and its implementation

---
+++ lib/src/domain/files/create_path/i_create_template_directory.dart
@@ -0,0 +1,17 @@
+import 'package:moncli/src/base/constants.dart';
+import 'package:moncli/src/domain/files/i_create_path.dart';
+
+abstract class ICreateTemplatesDirectory {
+  void call();
+}
+
+class CreateTemplatesDirectory implements ICreateTemplatesDirectory {
+  final IPathConstants pathConstants;
+  final ICreatePath createPath;
+
+  CreateTemplatesDirectory(this.pathConstants, this.createPath);
+
+  @override
+  void call() => createPath(pathConstants.templateFolderPath,
+      overwrite: false, isFile: false);
+}
