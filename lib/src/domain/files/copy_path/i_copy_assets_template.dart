5a324bc0fae322ad78ce85d7c7b6580b362945d3
Juan E Quintero R <juan@57blocks.com>
2021-06-19 08:26:44 -0500

    feat: create ICopyAssetsTemplate and its implementation

---
+++ lib/src/domain/files/copy_path/i_copy_assets_template.dart
@@ -0,0 +1,18 @@
+import 'package:moncli/src/base/constants.dart';
+import 'package:moncli/src/domain/files/i_copy_path.dart';
+
+abstract class ICopyAssetsTemplate {
+  void call({bool overwrite = false});
+}
+
+class CopyAssetsTemplate implements ICopyAssetsTemplate {
+  final IPathConstants pathConstants;
+  final ICopyFile copyFile;
+
+  CopyAssetsTemplate(this.pathConstants, this.copyFile);
+
+  @override
+  void call({bool overwrite = false}) =>
+      copyFile(pathConstants.assetsTemplatePath, pathConstants.assetsOutputPath,
+          overwrite: overwrite);
+}
