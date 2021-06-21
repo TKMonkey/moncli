5fe71a133cbc2870c25880d440e8893f34550ee7
Juan E Quintero R <juan@57blocks.com>
2021-06-21 06:51:02 -0500

    chore: update doc term

--- lib/src/models/templates/template.dart
+++ lib/src/models/templates/template.dart
@@ -2,7 +2,7 @@ import 'i_template.dart';
 
 /// Abstracts a template file.
 ///
-/// Shortcut for calling readAllElements() and validate() in constructor
+/// Shortcut for calling readAllNodes() and validate() in constructor
 abstract class Template implements ITemplate {
   Template() {
     readAllNodes();
