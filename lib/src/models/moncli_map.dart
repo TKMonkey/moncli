0b0638dca232c58e085e9f8ea0b4ebf2542ae852
Juan E Quintero R <juan@57blocks.com>
2021-06-29 08:41:32 -0500

    refactor: make code prettier

--- lib/src/models/moncli_map.dart
+++ lib/src/models/moncli_map.dart
@@ -1,6 +1,5 @@
 import 'dart:collection';
 
-import "package:meta/meta.dart";
 import 'package:moncli/src/base/exceptions/exceptions.dart';
 import 'package:moncli/src/models/node/i_node.dart';
 import 'package:moncli/src/models/node/i_null_node.dart';
@@ -11,43 +10,32 @@ import 'package:moncli/src/models/node/node_validator.dart';
 /// This subclass is in charge of storing mapped config files and
 /// returns INullNode instead of null value if no value is set for key
 class MoncliMap with MapMixin<String, INode> {
-  @protected
-  Map<String, INode> internalMap;
+  final Map<String, INode> _internalMap;
   final INullNode _nullNode;
 
   MoncliMap(this._nullNode, [Map<String, INode>? internalMap])
-      : internalMap = internalMap ?? Map.from({});
+      : _internalMap = internalMap ?? Map.from({});
 
   @override
-  INode operator [](Object? key) {
-    return internalMap.containsKey(key) ? internalMap[key]! : _nullNode;
-  }
+  INode operator [](Object? key) =>
+      containsKey(key) ? _internalMap[key]! : _nullNode;
 
   @override
-  void operator []=(String key, INode value) {
-    internalMap[key] = value;
-  }
+  void operator []=(String key, INode value) => _internalMap[key] = value;
 
   @override
-  void clear() {
-    internalMap.clear();
-  }
+  void clear() => _internalMap.clear();
 
   @override
-  Iterable<String> get keys => internalMap.keys;
+  Iterable<String> get keys => _internalMap.keys;
 
   @override
-  INode? remove(Object? key) {
-    return internalMap.remove(key);
-  }
+  INode? remove(Object? key) => _internalMap.remove(key);
 
-  T? getNodeAs<T extends INode>(String key) {
-    return this[key] as T?;
-  }
+  T? getNodeAs<T extends INode>(String key) => this[key] as T?;
 
-  T getNodeOrDefault<T extends INode>(String key, T defaultValue) {
-    return this[key] is INullNode ? defaultValue : this[key] as T;
-  }
+  T getNodeOrDefault<T extends INode>(String key, T defaultValue) =>
+      this[key] is INullNode ? defaultValue : this[key] as T;
 
   INode getNodeOrException(String key, {String? fileName}) {
     final value = this[key];
